9d6472bbb742b8692cfa9f855900f18afec423cf
Usman Siddiqui <usmansiddiqui51@gmail.com>
2025-06-23 22:58:29 +0500

    changed s3 bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform_3-tier-infra-bucket"
+    bucket         = "terraform-3-tier-infra-bucket"
     key            = "terraform/state"
     region         = "us-west-2"
     use_lockfile = true
